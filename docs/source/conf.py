c70124251e8a513c13e1b587f030d8e2f6457a91
Francesco Rubino <rubino.francesco@gmail.com>
2014-03-21 15:36:35 +0000

    Version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,7 +81,7 @@ copyright = u'2013, Francesco Rubino'
 #
 # The short X.Y version.
 
-__VERSION__ = '0.1.10'
+__VERSION__ = '0.1.11'
 
 version = __VERSION__
 # The full version, including alpha/beta/rc tags.
