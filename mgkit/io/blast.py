99d42b3fee3492c193a8d901d578fb5aa2d5dc1e
Francesco Rubino <rubino.francesco@gmail.com>
2014-05-27 17:29:04 +0100

    Added new function to parse blast output.

--- mgkit/io/blast.py
+++ mgkit/io/blast.py
@@ -11,8 +11,11 @@ NUM_LINES = 10**6
 LOG = logging.getLogger(__name__)
 
 
-def parse_blast_tab(f_handle, gid_col=1, score_col=11, num_lines=NUM_LINES):
+def _parse_blast_tab(f_handle, gid_col=1, score_col=11, num_lines=NUM_LINES):
     """
+    .. deprecated:: 0.1.12
+        Use the more general version :func:`parse_blast_tab`
+
     Parses blast output tab format, taking the best hit (first one) for each
     KO id (ko_idx). Returns a dictionary with ko_idx as key and the GID and bit
     score as value.
@@ -130,3 +133,86 @@ def add_blast_result_to_annotation(annotation, gi_taxa_dict, taxonomy,
         annotation.attributes.blast_taxon_idx = hit.taxon_id
     except KeyError:
         return
+
+
+def parse_blast_tab(file_handle, seq_id=0, ret_col=(0, 1, 2, 6, 7, 11),
+                    key_func=None, value_funcs=None):
+    """
+    .. versionadded:: 0.1.12
+
+    Parses blast output tab format, returning for each line a key (the query
+    id) and the columns requested in a tuple.
+
+    Arguments:
+        file_handle (file): file handle for the blast ouput
+        seq_id (int): index for the column which has the query id
+        ret_col (list, None): list of indexes for the columns to be returned or
+            *None* if all columns must be returned
+        key_func (None, func): function to transform the query id value in the
+            key returned. If *None*, the query id is used
+        value_funcs (None, list): list of functions to transform the value of
+            all the requested columns. If *None* the values are not converted
+
+    Returns:
+        iterator: iterator of tuples with the first element being the query id
+        after key_func is applied, if requested and the second element of the
+        tuple is a tuple with the requested columns *ret_col*
+
+    .. table:: BLAST+ used with `-outfmt 6`, default columns
+
+        +--------------+--------------------------------+
+        | column index |          description           |
+        +==============+================================+
+        |            0 | query name                     |
+        +--------------+--------------------------------+
+        |            1 | subject name                   |
+        +--------------+--------------------------------+
+        |            2 | percent identities             |
+        +--------------+--------------------------------+
+        |            3 | aligned length                 |
+        +--------------+--------------------------------+
+        |            4 | number of mismatched positions |
+        +--------------+--------------------------------+
+        |            5 | number of gap positions        |
+        +--------------+--------------------------------+
+        |            6 | query sequence start           |
+        +--------------+--------------------------------+
+        |            7 | query sequence end             |
+        +--------------+--------------------------------+
+        |            8 | subject sequence start         |
+        +--------------+--------------------------------+
+        |            9 | subject sequence end           |
+        +--------------+--------------------------------+
+        |           10 | e-value                        |
+        +--------------+--------------------------------+
+        |           11 | bit score                      |
+        +--------------+--------------------------------+
+
+    """
+
+    if key_func is None:
+        key_func = lambda x: x
+
+    if value_funcs is None:
+        value_funcs = [lambda x: x for y in range(len(ret_col))]
+
+    if ret_col is None:
+        ret_col = range(12)
+
+    for line in file_handle:
+        if line.startswith('#'):
+            continue
+
+        line = line.strip()
+
+        if not line:
+            continue
+
+        line = line.split('\t')
+        key = key_func(line[seq_id])
+        values = tuple(
+            func(line[index])
+            for index, func in zip(ret_col, value_funcs)
+        )
+
+        yield key, values
