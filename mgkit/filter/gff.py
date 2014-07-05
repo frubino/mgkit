8555d90f7e48672ab5129527f399faf709b0c2fd
Francesco Rubino <rubino.francesco@gmail.com>
2014-07-05 20:17:11 +0100

    New functions to filter annotations

--- mgkit/filter/gff.py
+++ mgkit/filter/gff.py
@@ -6,14 +6,17 @@ import operator
 import itertools
 from ..utils.common import average_length, between
 
+#------------------------------------------------
+#old filter code, most of it should be deprecated
+
 
 def choose_by_score(ann1, ann2):
     "the winner is the first element of the list"
     return sorted((ann1, ann2), key=operator.attrgetter('score'))
 
 
-def choose_annotation(ann1, ann2, threshold, only_same_gene=False,
-                      only_same_strand=True, score_func=choose_by_score):
+def _choose_annotation(ann1, ann2, threshold, only_same_gene=False,
+                       only_same_strand=True, score_func=choose_by_score):
     "Choos one annotation, based on the score"
     #if we don't want to exclude overlaps from different strands
     if only_same_strand:
@@ -52,7 +55,7 @@ def choose_annotation(ann1, ann2, threshold, only_same_gene=False,
 
 
 def filter_overlapping(annotations, threshold, same_gene, both_strands,
-                       choose_func=choose_annotation,
+                       choose_func=_choose_annotation,
                        score_func=choose_by_score):
     """
     Filters annotations checking how much they overlap, in which case the ones
@@ -160,3 +163,107 @@ def filter_by_hit_length(ko_len, perc, annotation):
         int(annotation.attributes.aa_from) + 1
     ) / avg_len
     return hit >= perc
+
+
+#---------------------------------------
+
+
+def choose_annotation(ann1, ann2, overlap=100, choose_func=None):
+    """
+    .. versionadded:: 0.1.12
+
+    Given two :class:`mgkit.io.gff.Annotation`, if one of of the two annotations
+    either is contained in the other or they overlap for at least a *overlap*
+    number of bases, *choose_func* will be applied to both. The result of
+    *choose_func* is the the annotation to be discarderd. It returns *None* if
+    the annotations should be both kept.
+
+    No checks are made to ensure that the two annotations are on the same
+        sequence and strand, as the *intersect* method of
+        :class:`mgkit.io.gff.Annotation` takes care of them.
+
+    Arguments:
+        ann1: instance of :class:`mgkit.io.gff.Annotation`
+        ann2: instance of :class:`mgkit.io.gff.Annotation`
+        overlap (int, float): number of bases overlap that trigger the filtering
+        choose_func (None, func): function that accepts *ann1* and *ann2* and
+            return the one to be discarded or None if both are accepted
+
+    Returns:
+        (None, Annotation): returns either the :class:`mgkit.io.gff.Annotation`
+        to be discarded or None, which is the result of *choose_func*
+
+    .. note::
+
+        If *choose_func* is *None*, the default function is used::
+
+            lambda a1, a2: min(a1, a2, key=lambda el: (el.dbq, el.bitscore, len(el)))
+
+        In order of importance the db quality, the bitscore and the length. The
+        annotation with the lowest tuple value is the one to discard.
+
+    """
+
+    if choose_func is None:
+        choose_func = lambda a1, a2: min(a1, a2, key=lambda el: (el.dbq, el.bitscore, len(el)))
+
+    intersect = ann1.intersect(ann2)
+
+    if intersect is not None:
+        #if the intersection is the same size of one of the annotations size,
+        #it means that one contain the other
+        if (len(intersect) == len(ann1)) or (len(intersect) == len(ann2)):
+            return choose_func(ann1, ann2)
+        else:
+            #if the overlap is longer than the threshold
+            if len(intersect) > overlap:
+                return choose_func(ann1, ann2)
+
+    return None
+
+
+def filter_annotations(annotations, choose_func=None, sort_func=None, reverse=True):
+    """
+    .. versionadded:: 0.1.12
+
+    Filter an iterable of :class:`mgkit.io.gff.Annotation` instances sorted
+    using *sort_func* as key in *sorted* and if the order is to be *reverse*;
+    it then applies *choose_func* on all possible pair combinations, using
+    itertools.combinations.
+
+    By default *choose_func* is :func:`choose_annotation` with the default
+    values, the list of annotation is sorted by bitscore, from the highest to
+    the lowest value.
+
+    Arguments:
+        annotations (iterable): iterable of :class:`mgkit.io.gff.Annotation`
+            instances
+        choose_func (func, None): function used to select the *losing*
+            annotation; if `None`, it will be :func:`choose_annotation` with
+            default values
+        sort_func (func, None): by default the sorting key is the bitscore of
+            the annotations
+        reverse (bool): passed to `sorted`, by default is reversed
+
+    Returns:
+        set: a set with the annotations that pass the filtering
+    """
+
+    if choose_func is None:
+        choose_func = choose_annotation
+
+    if sort_func is None:
+        sort_func = lambda x: x.bitscore
+
+    annotations = sorted(annotations, key=sort_func, reverse=reverse)
+
+    to_remove = set()
+
+    for ann1, ann2 in itertools.combinations(annotations, 2):
+
+        if ann1 in to_remove or ann2 in to_remove:
+            continue
+
+        to_remove.add(choose_func(ann1, ann2))
+
+    return set(annotations) - to_remove
