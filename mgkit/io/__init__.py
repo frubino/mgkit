2df22a31fffe500e14754014d80641cd1f2b188e
Francesco Rubino <rubino.francesco@gmail.com>
2014-07-10 17:13:13 +0100

    Added compressed_handle function in mgkit.io and fixed documentation (added version in which open_file was added)

--- mgkit/io/__init__.py
+++ mgkit/io/__init__.py
@@ -18,6 +18,8 @@ class UnsupportedFormat(IOError):
 
 def open_file(file_name, mode='r'):
     """
+    .. versionadded:: 0.1.12
+
     Opens a file using the extension as a guide to which module to use.
 
     Arguments:
@@ -42,5 +44,33 @@ def open_file(file_name, mode='r'):
             file_handle = lzma.LZMAFile(file_name, mode)
     else:
         file_handle = open(file_name, mode)
+
+    return file_handle
+
+
+def compressed_handle(file_handle):
+    """
+    .. versionadded:: 0.1.13
+
+    Tries to wrap a file handle in the appropriate compressed file class.
+
+    Arguments:
+        file_handle (str): file handle
+
+    Returns:
+        file: the same file handle if no suitable compressed file class is
+        found or the new file_handle which supports the compression
+
+    Raises:
+        UnsupportedFormat: if the module to open the file is not available
+
+    """
+    if file_handle.name.endswith('.gz'):
+        file_handle = gzip.GzipFile(fileobj=file_handle, mode='rb')
+    elif file_handle.name.endswith('.xz'):
+        if lzma:
+            raise UnsupportedFormat("Cannot import lzma module")
+        else:
+            file_handle = lzma.LZMAFile(file_handle)
 
     return file_handle
