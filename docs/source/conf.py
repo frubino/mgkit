82ba51c20eefa229d0412437cefd8d2e72711779
Francesco Rubino <rubino.francesco@gmail.com>
2015-01-22 17:43:14 +0000

    Added some patterns to the ignore list for hg and sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -106,7 +106,7 @@ release = __VERSION__
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['**/.ipynb_checkpoints/']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
