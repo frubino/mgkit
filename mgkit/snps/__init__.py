67a637a457b0dd01047f76d085b71feaa391fe1a
Francesco Rubino <rubino.francesco@gmail.com>
2015-03-15 14:07:08 +0000

    Fixed imports

--- mgkit/snps/__init__.py
+++ mgkit/snps/__init__.py
@@ -3,8 +3,8 @@ SNPs data package
 """
 
 #these import are for "legacy" scripts
-from .funcs import combine_snps_in_dataframe_test
-combine_snps_in_dataframe = combine_snps_in_dataframe_test
+#from .funcs import combine_snps_in_dataframe_test
+#combine_snps_in_dataframe = combine_snps_in_dataframe_test
 
 #imported to support old pickled data and simpler access
 from .classes import GeneSyn
@@ -12,3 +12,7 @@ from .classes import GeneSyn
 #imported for convenience
 from .conv_func import get_rank_dataframe, get_gene_map_dataframe
 from .funcs import combine_sample_snps
+
+import filter
+import funcs
+import conv_func
