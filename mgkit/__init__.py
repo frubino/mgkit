7216670de79bda01ff8a8e691db2f1e700e83eac
Francesco Rubino <rubino.francesco@gmail.com>
2015-04-07 16:39:11 +0100

    Added `--cite` option to most script, and a citation function in the module :func:`mgkit.cite`

--- mgkit/__init__.py
+++ mgkit/__init__.py
@@ -1,6 +1,8 @@
 """
 Metagenomics Framework
 """
+from __future__ import print_function
+import sys
 
 __VERSION__ = "0.1.16"
 
@@ -19,3 +21,25 @@ LOGO = """
  _|      _|    _|_|_|  _|    _|  _|      _|_|
 
 """
+
+CITE = """
+Rubino, F. and Creevey, C.J. (2014).
+MGkit: Metagenomic Framework For The Study Of Microbial Communities.
+
+Available at: http://figshare.com/articles/MGkit_Metagenomic_Framework_For_The_Study_Of_Microbial_Communities/1269288
+
+[doi:10.6084/m9.figshare.1269288]
+"""
+
+
+def cite(file=sys.stderr):
+    """
+    Print citation to the specified stream
+    """
+    print(
+        LOGO,
+        'MGKit Version: {0}'.format(__VERSION__),
+        CITE,
+        sep='\n',
+        file=file
+    )
