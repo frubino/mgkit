10d956a32f35ac850fe751d1cfab920de3f23acd
Francesco Rubino <rubino.francesco@gmail.com>
2015-04-07 16:39:44 +0100

    Fixed citation

--- mgkit/workflow/utils.py
+++ mgkit/workflow/utils.py
@@ -1,11 +1,32 @@
 """
 Utility functions for workflows
 """
+from __future__ import print_function
 import logging
+import argparse
 import sys
 import mgkit
 
 LOG = logging.getLogger(__name__)
+
+
+class CiteAction(argparse.Action):
+    def __init__(self,
+                 option_strings,
+                 dest=argparse.SUPPRESS,
+                 default=argparse.SUPPRESS,
+                 help='Show citation for the framework'):
+        super(CiteAction, self).__init__(
+            option_strings,
+            dest=dest,
+            default=default,
+            nargs=0,
+            help=help)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        mgkit.cite(file=sys.stderr)
+        setattr(namespace, self.dest, values)
+        parser.exit(0)
 
 
 def add_basic_options(parser):
@@ -19,6 +40,10 @@ def add_basic_options(parser):
         const=logging.DEBUG,
         default=logging.INFO,
         help='more verbose'
+    )
+    parser.add_argument(
+        '--cite',
+        action=CiteAction
     )
     parser.add_argument(
         '--version',
