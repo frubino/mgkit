0c0b8af36d1766af73a136d5a987d04f36e4e687
Francesco Rubino <rubino.francesco@gmail.com>
2015-04-24 17:28:50 +0100

    Changed style

--- mgkit/plots/abund.py
+++ mgkit/plots/abund.py
@@ -78,61 +78,28 @@ def draw_triangle_grid(ax, labels=['LAM', 'SAM', 'EAM'], linewidth=1.,
         x1, y1 = project_point((index, 1 - index, 0))
         x2, y2 = project_point((0, 1 - index, index))
         # EAM
-        ax.plot(
-            [x1, x2],
-            [y1, y2],
-            linestyle=styles[2],
-            color='k',
-            linewidth=linewidth
-        )
+        ax.plot([x1, x2], [y1, y2], linestyle=styles[2], color='k',
+                linewidth=linewidth)
 
         x3, y3 = project_point((0, index, 1 - index))
         x4, y4 = project_point((index, 0, 1 - index))
         # SAM
-        ax.plot(
-            [x3, x4],
-            [y3, y4],
-            linestyle=styles[1],
-            color='k',
-            linewidth=linewidth
-        )
+        ax.plot([x3, x4], [y3, y4], linestyle=styles[1], color='k',
+                linewidth=linewidth)
 
         # LAM
-        ax.plot(
-            [x4, x1],
-            [y4, y1],
-            linestyle=styles[0],
-            color='k',
-            linewidth=linewidth
-        )
+        ax.plot([x4, x1], [y4, y1], linestyle=styles[0], color='k',
+                linewidth=linewidth)
 
         # LAM labels
-        ax.text(
-            x1,
-            y1 - 0.02,
-            index,
-            va='top',
-            ha='center',
-            fontsize=int(fontsize * 0.75)
-        )
+        ax.text(x1, y1 - 0.02, index, va='top', ha='center',
+                fontsize=int(fontsize * 0.75))
         # EAM labels
-        ax.text(
-            x4 - 0.02,
-            y4,
-            1 - index,
-            ha='right',
-            va='center',
-            fontsize=int(fontsize * 0.75)
-        )
+        ax.text(x4 - 0.02, y4, 1 - index, ha='right', va='center',
+                fontsize=int(fontsize * 0.75))
         # EAM labels
-        ax.text(
-            x3 + 0.02,
-            y3,
-            index,
-            ha='left',
-            va='center',
-            fontsize=int(fontsize * 0.75)
-        )
+        ax.text(x3 + 0.02, y3, index, ha='left', va='center',
+                fontsize=int(fontsize * 0.75))
 
 
 def draw_1d_grid(ax, labels=['LAM', 'SAM'], fontsize=22):
@@ -153,22 +120,8 @@ def draw_1d_grid(ax, labels=['LAM', 'SAM'], fontsize=22):
     ax.set_xlim((-0.1, 1.1))
     ax.set_axis_off()
 
-    ax.text(
-        -0.025,
-        0,
-        labels[0],
-        fontsize=fontsize,
-        va='center',
-        ha='right'
-    )
-    ax.text(
-        1.025,
-        0,
-        labels[1],
-        fontsize=fontsize,
-        va='center',
-        ha='left'
-    )
+    ax.text(-0.025, 0, labels[0], fontsize=fontsize, va='center', ha='right')
+    ax.text(1.025, 0, labels[1], fontsize=fontsize, va='center', ha='left')
 
     ty = 1.0
     by = -1.0
@@ -177,22 +130,9 @@ def draw_1d_grid(ax, labels=['LAM', 'SAM'], fontsize=22):
 
     for x in numpy.arange(0.1, 1., 0.1):
         ax.plot([x, x], [by, ty], color='k', linestyle=':')
-        ax.text(
-            x,
-            by - .1,
-            x,
-            fontsize=fontsize * 0.75,
-            ha='center',
-            va='top'
-        )
-        ax.text(
-            x,
-            ty + .1,
-            1 - x,
-            fontsize=fontsize * 0.75,
-            ha='center',
-            va='bottom'
-        )
+        ax.text(x, by - .1, x, fontsize=fontsize * 0.75, ha='center', va='top')
+        ax.text(x, ty + .1, 1 - x, fontsize=fontsize * 0.75, ha='center',
+                va='bottom')
 
 
 def project_point(point):
