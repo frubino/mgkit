6b125ff0d8901e5a00978bd59754c3cb58aff7f3
Francesco Rubino <rubino.francesco@gmail.com>
2015-11-20 11:33:26 +1000

    Changing the tutorial - midway through

--- docs/source/examples/tutorial-hmmer.sh
+++ docs/source/examples/tutorial-hmmer.sh
@@ -1,136 +1,161 @@
 #!/bin/bash
 
-#download data
-#50 meters
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR001/SRR001326/SRR001326.fastq.gz
-#1 meter
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR001/SRR001325/SRR001325.fastq.gz
-#32 meters
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR001/SRR001323/SRR001323.fastq.gz
-#16 meters
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR001/SRR001322/SRR001322.fastq.gz
-#uncompress data
-gunzip -v *.fastq.gz
+# Some tools require a contact email
+export EMAIL=your@email
 
-#assembly - preparatory phase
-velveth velvet_work 31 -fmtAuto  *.fastq
-#assembly
-velvetg velvet_work -min_contig_lgth 50
-#change sequence names
-cat velvet_work/contigs.fa | sed -E 's/(>NODE_[0-9]+)_.+/\1/g' > assembly.fa
+# Requirements
+#
+# software 	- version tested
+# mgkit 	- 0.2.1
+# wget 		- any
+# velvet 	- 1.2.10
+# bowtie2 	- 2.2.6 / 2.1.0
+# khmer 	- 2.0
+# hmmer 	- 3.1b2 / 3.1b1
+# clustalo 	- 1.2.1
+
+# Requirements specific for Mac OS X (El Capitan, 10.11): wget
+# brew install wget velvet bowtie2 samtools pyenv-virtualenv hmmer clustal-omega
+
+# Memory ~6 GB for khmer normalisation
+# ~9 GB for patitioning
+# ~3 GB for velveth
+# ~1.5 GB for velvetg
+# ~3.5 GB for megahit
+
+# Using data from the following project:
+# http://www.ebi.ac.uk/ena/data/view/PRJEB6461
+# Samples:
+# I (Influent)
+# B (Buffering)
+# SA (Secondary aeration)
+# PA (Primary aeration)
+# SD (Sludge digestion)
+mkdir seqs
+cd seqs
+#download data
+wget ftp://ftp.sra.ebi.ac.uk/vol1/ERA315/ERA315794/fastq/B_R1.fastq.gz ftp://ftp.sra.ebi.ac.uk/vol1/ERA315/ERA315794/fastq/B_R2.fastq.gz ftp://ftp.sra.ebi.ac.uk/vol1/ERA315/ERA315794/fastq/I_R1.fastq.gz ftp://ftp.sra.ebi.ac.uk/vol1/ERA315/ERA315794/fastq/I_R2.fastq.gz ftp://ftp.sra.ebi.ac.uk/vol1/ERA315/ERA315794/fastq/PA_R1.fastq.gz ftp://ftp.sra.ebi.ac.uk/vol1/ERA315/ERA315794/fastq/PA_R2.fastq.gz ftp://ftp.sra.ebi.ac.uk/vol1/ERA315/ERA315794/fastq/SA_R1.fastq.gz ftp://ftp.sra.ebi.ac.uk/vol1/ERA315/ERA315794/fastq/SA_R2.fastq.gz ftp://ftp.sra.ebi.ac.uk/vol1/ERA315/ERA315794/fastq/SD_R1.fastq.gz ftp://ftp.sra.ebi.ac.uk/vol1/ERA315/ERA315794/fastq/SD_R2.fastq.gz
+
+# Normalisation of the reads
+# Khmer when using paired reads works best when are interleaved
+# Moreover, it requires the older Casava type of header
+# The sequences are trimmed by 20 bp and fed to the khmer
+# script interleave-reads.py
+interleave-reads.py --gzip -o all-interleaved.fq.gz \
+<(
+python - <<END
+import HTSeq, sys, glob
+files = glob.glob('*R1.fastq.gz')
+for fname in files:
+    for record in HTSeq.FastqReader(fname):
+        record = record[:-20] # cut 20 bp
+        # HTSeq adds '[part]' to the header, the next line cut it
+        record.name = record.name[:-6]
+        record.write_to_fastq_file(sys.stdout)
+END
+) \
+<(
+python - <<END
+import HTSeq, sys, glob
+files = glob.glob('*R2.fastq.gz')
+for fname in files:
+    for record in HTSeq.FastqReader(fname):
+        record = record[:-20]
+        record.name = record.name[:-6]
+        record.write_to_fastq_file(sys.stdout)
+END
+)
+# Normalisation takes out almost 7% of the reads
+normalize-by-median.py -k 24 -p -o normalised.fq.gz --gzip -M 6e9 all-interleaved.fq.gz
+# rm all-interleaved.fq.gz
+
+megahit --presets meta --verbose -t 2 --min-contig-len 50 --12 normalised.fq.gz -o megahit-out
+
+cd ..
+
+# Get the nitrogen metabolism KOs
+export GENES=`python - <<END
+from mgkit import kegg
+k = kegg.KeggClientRest()
+print " ".join(k.link_ids('ko', 'ko00910')['ko00910'])
+END`
+
+#download offline data (only taxonomy)
+download_data -p -x -m $EMAIL
+
+download_profiles -o profiles -ko $GENES -r order -l bacteria -m $EMAIL -t mg_data/taxonomy.pickle
+
+unset GENES
+
+#Profile alignments and build
+for file in profiles/*.fa; do
+	echo $file `basename $file .fa`
+    clustalo -i $file -o profiles/`basename $file .fa`.afa ;
+    hmmbuild profiles/`basename $file .afa`.hmm profiles/`basename $file .fa`.afa;
+done
+#Make one file for all profiles
+cat profiles/*.hmm > profiles.hmm
 
 #translation of the assembly in AA
 translate_seq assembly.fa assembly.aa.fa
 
-#download offline data
-download_data -p -m EMAIL
-
-#download profile for the three taxa
-download_profiles -o bacteria_profiles -i 200795 201174 203682 -m EMAIL -k mg_data/kegg.pickle -t mg_data/taxonomy.pickle
-#and archaea
-download_profiles -o archaea_order_profiles -r order -l archaea -m EMAIL -k mg_data/kegg.pickle -t mg_data/taxonomy.pickle
-download_profiles -o archaea_phylum_profiles -r phylum -l archaea -m EMAIL -k mg_data/kegg.pickle -t mg_data/taxonomy.pickle
-
-#Profile alignments and build
-for file in bacteria_profiles/*.fa; do
-        clustalo -i $file -o bacteria_profiles/`basename $file .fa`.afa;
-        hmmbuild bacteria_profiles/`basename $file .afa`.hmm $file;
-done
-for file in archaea_order_profiles/*.fa; do
-        clustalo -i $file -o archaea_order_profiles/`basename $file .fa`.afa;
-        hmmbuild archaea_order_profiles/`basename $file .afa`.hmm $file;
-done
-for file in archaea_phylum_profiles/*.fa; do
-        clustalo -i $file -o archaea_phylum_profiles/`basename $file .fa`.afa;
-        hmmbuild archaea_phylum_profiles/`basename $file .afa`.hmm $file;
-done
-#Make one file for all profiles
-cat profile_files/*.hmm archaea_order_profiles/*.hmm archaea_phylum_profiles/*.hmm > profile_files.hmm
-
 #HMMER command line
-hmmsearch -o /dev/null --domtbl hmmer_dom-table.txt profile_files.hmm assembly.aa.fa
+hmmsearch -o /dev/null --domtbl hmmer_dom-table.txt profiles.hmm assembly.aa.fa
 #convert into annotations
-hmmer2gff -o assembly.gff -k mg_data/kegg.pickle -n assembly.fa assembly.aa.fa hmmer_dom-table.txt
+hmmer2gff -d -o assembly.gff assembly.aa.fa hmmer_dom-table.txt
 #Filter annotations
-filter_gff -l -i assembly.gff -o assembly.filt.gff
-
-#extract aa sequences
-#python -c "import mgkit;mgkit.logger.config_log(); import mgkit.io.gff; mgkit.io.gff.extract_aa_seqs('assembly.filt.gff', 'assembly.aa.pred.fa')"
-#blastp
-#blastp -query assembly.aa.pred.fa -db nr -out assembly.aa.pred.blast.tab -outfmt 7 -num_threas 8
+filter-gff overlap -s 100 assembly.gff assembly.filt.gff
 
 #bowtie2
 bowtie2-build assembly.fa assembly.fa
-for file in *.fastq; do
-	BASENAME=`basename $file .fastq`
-	bowtie2 -N 1 -x assembly.fa -U $file \
-	--rg-id $BASENAME --rg PL:454 --rg PU:454 \
-	--rg SM:$BASENAME| samtools view -Sb - > $BASENAME.bam;
+for file in R1.fastq.gz; do
+	BASENAME=`basename $file _R1.fastq.gz`
+	bowtie2 -N 1 -x assembly.fa -1 $file -2 $file_R2.fastq.gz \
+	--rg-id $BASENAME --rg PL:Illumina --rg PU:Illumina-MiSeq \
+	--rg SM:$BASENAME | samtools view -Sb - > $BASENAME.bam;
 done
 
 #samtools
-for file in *.bam; do 
+for file in *.bam; do
 	samtools sort $file `basename $file .bam`-sort;
 	samtools index `basename $file .bam`-sort.bam;
 	#removes the unsorted file, it's not needed
 	rm $file;
 done
 
-#index for the assembly (required by GATK and samtools) 
+#index for the assembly (required by GATK and samtools)
 #.fai index
 samtools faidx assembly.fa
-#.dict index;
-#if there's an error execute: execstack -c picard-tools/libIntelDeflater.so
-java -jar picard-tools/CreateSequenceDictionary.jar R=assembly.fa O=assembly.dict
 
 #add coverage data
 export SAMPLES=$(for file in *.bam; do echo -a `basename $file -sort.bam`,$file ;done)
 add_coverage_to_gff -v $SAMPLES assembly.filt.gff assembly.filt.cov.gff
-unset SAMPLES
 
-#SNP calling using GATK
-# #GATK
-# for file in *-sort.bam; do
-
-# 	  java -jar GATK/GenomeAnalysisTK.jar -R assembly.fa \
-# 	  -T UnifiedGenotyper -I $file \
-# 	  -o `basename $file .bam`.vcf -dcov 50;
-
-# done
-
-#SNP calling using samtools 
-for file in *.bam; do 
-	samtools mpileup -Iuf assembly.fa \
-	$file |bcftools view -vcg - > `basename $file`.vcf;
+#SNP calling using samtools
+for file in *.bam; do
+	samtools mpileup -Iuvf assembly.fa $file > `basename $file`.vcf;
 done
+
+samtools mpileup -t DP,DV -s -q 10 -Iuvf assembly.fa *.bam
+
+#fasta .dict file, needed for GATK
+wget https://github.com/broadinstitute/picard/releases/download/1.140/picard-tools-1.140.zip
+unzip picard-tools-1.140.zip
+java -jar picard-tools-1.140/picard.jar CreateSequenceDictionary \
+	R=assembly.fa O=assembly.dict
 
 #merge vcf
 export SAMPLES=$(for file in *-sort.bam.vcf; do echo -V:`basename $file -sort.bam.vcf` $file ;done)
-java -Xmx10g -jar GATK/GenomeAnalysisTK.jar \
+java -Xmx1g -jar GATK/GenomeAnalysisTK.jar \
 	  -R assembly.fa -T CombineVariants -o assembly.vcf \
 	  -genotypeMergeOptions UNIQUIFY \
 	  $SAMPLES
 unset SAMPLES
 
-#convert to gtf
-python -c "import mgkit;mgkit.logger.config_log(); import mgkit.io.gff; mgkit.io.gff.convert_gff_to_gtf('assembly.filt.gff', 'assembly.filt.gtf')"
-
-#Download SNPdat
-https://snpdat.googlecode.com/files/SNPdat_v1.0.5.pl
-
-#SNPdat
-for file in *SR*.vcf; do 
-	perl SNPdat_v1.0.5.pl -i $file -g assembly.filt.gtf \
-	-f assembly.fa -o `basename $file .vcf`.snpdat;
-done
-
 #snp_parser
-export SAMPLES=$(for file in *.bam; do echo `basename $file -sort.bam`;done)
-export SNPDAT_FILES=*.snpdat
-snp_parser -v -b -g assembly.filt.cov.gff \
+export SAMPLES=$(for file in *.bam; do echo -m `basename $file .bam`;done)
+snp_parser -v -g assembly.uniprot.gff \
 	-p assembly.vcf \
- 	-s $SNPDAT_FILES \
- 	-m $SAMPLES \
- 	-t mg_data/taxonomy.pickle
+	-a assembly.fasta \
+ 	$SAMPLES
 unset SAMPLES
-unset SNPDAT_FILES
