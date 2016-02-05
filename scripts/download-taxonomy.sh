2ec0cbadd7d7daca8a86866b218da13b185d1714
Francesco Rubino <rubino.francesco@gmail.com>
2016-02-05 18:14:49 +1000

    Added possibility to change the taxonomy file name (msgpack can be used instead of pickle, depending on the file extension)

--- scripts/download-taxonomy.sh
+++ scripts/download-taxonomy.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+# checks if the file was already downloaded
 if [ ! -f taxdump.tar.gz ]; then
     wget ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
 fi
+
+# checks if a file name for the taxonomy file was passed as argument
+if [ -z "$1" ];
+	then
+		TAXONOMY=taxonomy.pickle
+	else
+		TAXONOMY=$1
+fi
+
+echo $TAXONOMY > taxonomy_fname
 
 DUMP_DIR=taxdump/
 
@@ -23,12 +34,12 @@ from mgkit.taxon import UniprotTaxonomy
 mgkit.logger.config_log()
 
 files = [line.strip() for line in open('files.txt', 'r')]
-    
+
 taxonomy = UniprotTaxonomy()
 taxonomy.read_from_ncbi_dump(*files)
-taxonomy.save_data('taxonomy.pickle')
+taxonomy.save_data(open("taxonomy_fname", 'r').read().strip())
 END
 
 rm files.txt
+rm taxonomy_fname
 rm -R $DUMP_DIR
-
