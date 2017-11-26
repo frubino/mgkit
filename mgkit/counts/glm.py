74a84646b66efe59d8d9bab2a454b4325fb0aba2
Francesco Rubino <rubino.francesco@gmail.com>
2017-11-26 21:06:00 +1000

    Continued work on glm module

--- mgkit/counts/glm.py
+++ mgkit/counts/glm.py
@@ -14,7 +14,7 @@ except ImportError:
 
 
 def lowess_ci_bootstrap(endog, exog, num=100, frac=.2, it=3, alpha=.05,
-                        delta=0.):
+                        delta=0., min_alpha=10**-3, kind='slinear'):
     """
     Performance increase with the value of *delta*.
     """
@@ -46,11 +46,18 @@ def lowess_ci_bootstrap(endog, exog, num=100, frac=.2, it=3, alpha=.05,
     q1 = boots.groupby('exog').quantile(
         alpha, interpolation='nearest'
     ).sort_index()
+    q1 = q1.endog[q1.endog > 0].fillna(min_alpha)
+    q1 = interpolate.interp1d(q1.index, q1, kind='slinear', fill_value='extrapolate')
+
     q2 = boots.groupby('exog').quantile(
         1 - alpha, interpolation='nearest'
     ).sort_index()
-    return q1.endog, q2.endog
+    q2 = q2.endog[q2.endog > 0].fillna(min_alpha)
+    q2 = interpolate.interp1d(q2.index, q2, kind='slinear', fill_value='extrapolate')
 
+    m = fit_lowess_interpolate(data.endog, data.exog, frac=frac, it=it, kind=kind)
+
+    return q1, q2, m
 
 def fit_lowess_interpolate(endog, exog, frac=.2, it=3, kind='slinear'):
     data = pd.DataFrame(
@@ -71,13 +78,17 @@ def fit_lowess_interpolate(endog, exog, frac=.2, it=3, kind='slinear'):
         lw[:, 0],
         lw[:, 1],
         kind=kind,
+        fill_value='extrapolate'
     )
 
 
-def variance_to_alpha(mu, func):
+def variance_to_alpha(mu, func, min_alpha=10**-3):
     """
     Based on the variance defined in the Negative Binomial in statsmodels
 
     var = mu + alpha * (mu ** 2)
     """
-    return (func(mu) - mu) / (mu ** 2)
+
+    alpha = (func(mu) - mu) / (mu ** 2)
+
+    return min_alpha if alpha <= 0 else alpha
