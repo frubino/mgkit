5dec976bdcf111234144258d02011638b2a9b4ce
Francesco Rubino <rubino.francesco@gmail.com>
2018-05-07 09:17:07 +0200

    Skip network tests if no connection available

--- tests/test_net_pfam.py
+++ tests/test_net_pfam.py
@@ -1,10 +1,12 @@
 import pytest
-
+from conftest import skipif
 from mgkit.net.pfam import get_pfam_families
 
+@skipif
 def test_pfam_families_id():
     assert 'Protamine_P1' in get_pfam_families(key='id')
 
 
+@skipif
 def test_pfam_families_ac():
     assert 'PF07938' in get_pfam_families(key='ac')
