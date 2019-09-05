e270fca8d4ffa76ce2eaa97faa2818b3690eb22f
Francesco Rubino <rubino.francesco@gmail.com>
2019-09-05 16:51:47 +0200

    Updated minimum pandas version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from setuptools import setup, find_packages
 
 install_requires = [
     'numpy>=1.9.2',
-    'pandas>=0.18',
+    'pandas>=0.24',
     'progressbar2',
     'tqdm>=4.0',
     'HTSeq>=0.9.1',
