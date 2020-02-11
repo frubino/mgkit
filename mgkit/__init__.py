d6b18f1fe828287cde4b9db83cc2c5731f864617
Francesco Rubino <rubino.francesco@gmail.com>
2020-02-11 12:12:03 +0000

    Version bump to 0.4.3

--- mgkit/__init__.py
+++ mgkit/__init__.py
@@ -7,7 +7,7 @@ import logging
 
 from . import logger
 
-__VERSION__ = "0.4.2"
+__VERSION__ = "0.4.3"
 
 __version__ = __VERSION__
 
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if USE_CYTHON:
     from Cython.Build import cythonize
     extensions = cythonize(extensions)
 
-__VERSION__ = "0.4.2"
+__VERSION__ = "0.4.3"
 
 from setuptools import setup, find_packages
 
